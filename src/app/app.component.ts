d7e44a570f1b06333f82aafd0e6866b81f71b370
Sipos Miklos <commit@siposm.hu>
2026-07-29 18:29:28 +0200

    app.ts API call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'demo';
+  constructor(public apiService: ApiService) {
+		this.apiService.getData()
+	}
 }
