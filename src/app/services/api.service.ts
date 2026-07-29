a1ecba7174ed0b17a185925aa5130151f429f9c8
Sipos Miklos <commit@siposm.hu>
2026-07-29 18:29:10 +0200

    service updated with API call (getDevelopers)

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { Developer } from '../models/developer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+	public developers: Developer[] = []
 
-  constructor() { }
+	constructor(private http: HttpClient) {}
+
+	getData() {
+		this.http.get<Developer[]>(environment.apiUlr).subscribe(data => {
+			this.developers = data as Developer[]
+		})
+	}
 }
